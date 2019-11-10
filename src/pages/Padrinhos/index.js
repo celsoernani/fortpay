8aeb584d79aad2bc0bd4a22cb810ac10b11617f2
celsoernani <celsoernane@gmail.com>
2019-11-10 05:27:59 -0300

    fodaci

--- src/pages/Padrinhos/index.js
+++ src/pages/Padrinhos/index.js
@@ -1,11 +1,73 @@
 import React, {Component} from 'react';
 
-import {View} from 'react-native';
+import {View, FlatList, Text, TouchableOpacity} from 'react-native';
 
-// import { Container } from './styles';
-
+import {
+  Container,
+  Header,
+  Avatar,
+  TextName,
+  Card,
+  TextSubtitle,
+  TextCard,
+} from './styles';
+const data = [
+  {
+    id: 0,
+    name: 'Luan Borges',
+    image: 'https://avatars1.githubusercontent.com/u/50432110?s=460&v=4',
+  },
+];
 export default class Padrinhos extends Component {
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '24%',
+        }}
+      />
+    );
+  };
+
   render() {
-    return <View />;
+    return (
+      <Container>
+        <View
+          style={{
+            flex: 2,
+            backgroundColor: '#138646',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              alignSelf: 'center',
+              marginTop: 30,
+              color: 'white',
+            }}>
+            Padrinho
+          </Text>
+        </View>
+        <Avatar source={{uri: data[0].image}} />
+        <TextName>{data[0].name}</TextName>
+
+        <View
+          style={{
+            marginTop: '20%',
+            flex: 7,
+          }}>
+          <Card>
+            <TextCard>{'Sobre'}</TextCard>
+            <TextSubtitle>
+              {
+                'Meu sobrinho que me ajuda a gerar meus boletos, ver e me avisar sobre meus vencimentos, além de me ajudar a entrar no mundo digital.'
+              }
+            </TextSubtitle>
+          </Card>
+        </View>
+      </Container>
+    );
   }
 }
---
+++ src/pages/Padrinhos/styles.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+export const TitleContainer = styled.View`
+  align-items: center;
+  margin: 30px;
+`;
+export const Header = styled.View`
+  background-color:#138646
+  height:${getStatusBarHeight(true) + 50}
+`;
+export const Avatar = styled.Image`
+  width: 100px;
+  margin-top: 65;
+  margin-left: 10px;
+  height: 100px;
+  border-radius: 50px;
+  position: absolute;
+  border: 5px solid #d9d9d9;
+`;
+export const Card = styled.View`
+border-width: 1;
+border-radius: 2;
+border-color: #ddd;
+border-bottom-width: 0;
+shadow-color: #000;
+shadow-offset: {width: 0, height: 2};
+shadow-opacity: 0.8;
+shadow-radius: 2;
+elevation: 1;
+margin-left: 5;
+margin-right: 5;
+margin-top: 10;
+`;
+
+export const TextName = styled.Text`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  letter-spacing: 0.025;
+  margin-left: 180px;
+  color: #00000099;
+`;
+export const TextCard = styled.Text`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  letter-spacing: 0.025;
+  color: #00000099;
+  padding: 5px;
+`;
+
+export const TextSubtitle = styled.Text`
+  font-style: normal;
+  font-size: 15px;
+  letter-spacing: 0.025;
+  color: #00000099;
+  padding: 5px;
+`;
+export const ChatLabel = styled.Text`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.4px;
+`;
+export const IconContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-width: 1px;
+  border-radius: 4px;
+  padding-horizontal: 10px;
+  padding-vertical: 5px;
+  border-color: #138646;
+`;
+export const Title = styled.Text`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 24px;
+  letter-spacing: 0.18px;
+  width: 300px;
+`;
