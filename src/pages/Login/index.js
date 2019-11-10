d88cf56dda0d8e64a9c8a41fefabb758057590ed
Jose Alves <jose.alves@itegra.com.br>
2019-11-10 01:31:53 -0300

    camera'

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,14 +3,17 @@ import {
   View,
   Text,
   TouchableOpacity,
-  Image
+  Image,
+  StyleSheet,
+  Modal
 } from 'react-native';
-
+import QRCodeScanner from 'react-native-qrcode-scanner';
 import { Container, Form, Input, Submit, SubmitText, Loading } from './styles';
 
 export default class Login extends Component {
   state = {
-    loading: false
+    loading: false,
+    color: 'red'
   }
   handleLogin = () => {
     this.setState({ loading: true })
@@ -19,6 +22,12 @@ export default class Login extends Component {
     }, 2000);
 
   }
+
+  onSuccess = (e) => {
+    this.setState({color: 'black'})
+
+  }
+
   render() {
     return (
       <Container>
@@ -26,7 +35,20 @@ export default class Login extends Component {
           // resizeMethod='resize'
           style={{ width: 200, height: 100, marginTop: 30 }}
           source={require('../../images/logo.png')} />
-
+        <QRCodeScanner
+        onRead={this.onSuccess}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+      <Text style={{color: this.state.color}}>Teste</Text>
         <Form>
           <Input
             // value={username}
@@ -52,3 +74,23 @@ export default class Login extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
